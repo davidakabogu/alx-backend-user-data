ad00be78e09f3bc259f250d985680c2e2d339445
DavidAkabogu <Akabogudavid@gmail.com>
2024-01-16 20:02:38 +0100

    include auth files

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,27 @@
+from flask import request
+from typing import List, TypeVar
+
+class Auth:
+    """
+    Manages the API authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Placeholder method for authentication requirement.
+        Returns False for demonstration purposes.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Placeholder method for retrieving authorization header.
+        Returns None for demonstration purposes.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Placeholder method for retrieving current user.
+        Returns None for demonstration purposes.
+        """
+        return None
