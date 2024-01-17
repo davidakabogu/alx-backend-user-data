1197d4fc7859c6a4a929248c5f8ec7bfdcb2ecba
DavidAkabogu <Akabogudavid@gmail.com>
2024-01-17 19:29:11 +0100

    minor update

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -65,5 +65,5 @@ class Auth:
         """
         if request is None:
             return None
-        session_name = os.getenv('SESSION_NAME')
+        session_name = os.environ.get('SESSION_NAME', "_my_session_id")
         return request.cookies.get(session_name)
